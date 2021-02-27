e5a369b315f462553fccc7ef1ff994ee7589c7a8
Gabriela Tchian <gabriela.tchian@dextra-sw.com>
2021-02-27 13:48:47 -0300

    final da aula 1

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -1,9 +1,23 @@
 defmodule Rocketpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read("#{filename}.csv")
-    handle_file(file)
+    "#{filename}.csv"
+    # pega o retorno da linha anterior e passa como primeiro argumento para essa
+    |> File.read()
+    |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      # o enum executa na hora e o stream é um operador lazy -> só executa qnd precisa do resultado
+      # |> Enum.map(fn number -> String.to_integer(number) end)
+      |> Stream.map(fn number -> String.to_integer(number) end)
+      |> Enum.sum()
+
+    # retorna sempre a última linha
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
 end
